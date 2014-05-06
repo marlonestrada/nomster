abb17ad929187dffe9859a708d83462fcfea99bf
Marlon Estrada <marlon.estrada@gmail.com>
2014-05-06 13:14:33 +0000

    Created comments model.

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
+  
   validates :name, :presence => true, :length => {minimum: 3, too_short: "Must have at least %{count} letters"}
   validates :address, :presence => true
   validates :description, :presence => true
